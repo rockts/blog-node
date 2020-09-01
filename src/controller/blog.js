3f8aa3661981303f9cf98d4fd88cca4708372377
rockts <rockts@sina.com>
2020-09-01 21:47:16 +0800

    添加几条测试数据

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -14,6 +14,27 @@ const getList = (author, keyword) => {
       content: '白日依山尽，黄河入海流。欲穷千里目，更上一层楼。',
       createTime: 1598964604902,
       author: '王之涣'
+    },
+    {
+      id: 3,
+      title: '新嫁娘词',
+      content: '白三日入厨下，洗手作羹汤。未谙姑食性，先遣小姑尝。',
+      createTime: 1598967923939,
+      author: '王建'
+    },
+    {
+      id: 4,
+      title: '相思',
+      content: '红豆生南国，春来发几枝。愿君多采撷，此物最相思。。',
+      createTime: 1598964604902,
+      author: '王维'
+    },
+    {
+      id: 5,
+      title: '杂诗',
+      content: '君自故乡来，应知故乡事。来日绮窗前，寒梅著花未？',
+      createTime: 1598967910447,
+      author: '王维'
     }
   ]
 }
