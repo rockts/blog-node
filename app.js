1f454ad4b89efa5918bbeaa9213c53fff1662a6d
rockts <rockts@sina.com>
2020-09-01 20:10:38 +0800

    添加 router

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-const handleBlogRouter = require("./src/router/blog");
-const handleUserRouter = require("./src/router/user");
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
 
 const serverHandle = (req, res) => {
   // 设置返回格式 JSON
-  res.setHeader("Content-type", "application/json");
+  res.setHeader('Content-type', 'application/json')
 
   // 获取 path
   const url = req.url;
-  req.path = url.split("?")[0];
+  req.path = url.split('?')[0];
 
   // 处理 blog 路由
-  const blogData = handleBlogRouter(req, res);
+  const blogData = handleBlogRouter(req, res)
   if (blogData) {
-    res.end(JSON.stringify(blogData));
-    return;
+    res.end(
+      JSON.stringify(blogData)
+    )
+    return
   }
 
   // 处理 user 路由
-  const userData = handleUserRouter(req, res);
+  const userData = handleUserRouter(req, res)
   if (userData) {
-    res.end(JSON.stringify(userData));
-    return;
+    res.end(
+      JSON.stringify(userData)
+    )
+    return
   }
 
   // 未命中路由，返回 404
-  res.writeHead(404, { "Content-type": "text/plain" });
-  res.write("404 Not Found\n");
-  res.end();
-};
+  res.writeHead(404, { 'Content-type': 'text/plain' })
+  res.write('404 Not Found\n')
+  res.end()
+}
 
-module.exports = serverHandle;
+module.exports = serverHandle
 
 // process.env.NODE_ENV
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,40 +1,42 @@
 const handleBlogRouter = (req, res) => {
-  const method = req.method; // GET POST
+  const method = req.method // GET POST
+  const url = req.url
+  const path = url.split('?')[0]
 
   // 获取博客列表
-  if (method === "GET" && req.path === "/api/blog/list") {
+  if (method === 'GET' && req.path === '/api/blog/list') {
     return {
-      msg: "这是获取博客列表到接口",
-    };
+      msg: '这是获取博客列表到接口'
+    }
   }
 
   // 获取博客详情
-  if ((method === "GET" && req.path = "/api/blog/detail")) {
+  if (method === 'GET' && req.path === '/api/blog/detail') {
     return {
-      msg: "这是获取博客详情到接口",
-    };
+      msg: '这是获取博客详情到接口'
+    }
   }
 
   // 新建一篇博客
-  if (method === "POST" && req.path === "/api/blog/new") {
+  if (method === 'POST' && req.path === '/api/blog/new') {
     return {
-      msg: "这是新建博客的接口",
-    };
+      msg: '这是新建博客的接口'
+    }
   }
 
   // 更新一篇博客
-  if (method === "POST" && req.path === "/api/blog/update") {
+  if (method === 'POST' && req.path === '/api/blog/update') {
     return {
-      msg: "这是更新博客的接口",
-    };
+      msg: '这是更新博客的接口'
+    }
   }
 
-  // 新建一篇博客
-  if (method === "POST" && req.path === "/api/blog/del") {
+  // 删除一篇博客
+  if (method === 'POST' && req.path === '/api/blog/del') {
     return {
-      msg: "这是删除博客的接口",
-    };
+      msg: '这是删除博客的接口'
+    }
   }
-};
+}
 
-module.exports = handleBlogRouter;
+module.exports = handleBlogRouter
