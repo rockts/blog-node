e62f1b2adfff6e02eb9a9c4e8f7fef53cf8bcbe9
rockts <rockts@sina.com>
2020-09-16 20:17:46 +0800

    api对接mysql博客列表页

--- app.js
+++ app.js
@@ -48,11 +48,20 @@ const serverHandle = (req, res) => {
     req.body = postData
 
     // 处理 blog 路由
-    const blogData = handleBlogRouter(req, res)
-    if (blogData) {
-      res.end(
-        JSON.stringify(blogData)
-      )
+    // const blogData = handleBlogRouter(req, res)
+    // if (blogData) {
+    //   res.end(
+    //     JSON.stringify(blogData)
+    //   )
+    //   return
+    // }
+    const blogResult = handleBlogRouter(req, res)
+    if (blogResult) {
+      blogResult.then(blogData => {
+        res.end(
+          JSON.stringify(blogData)
+        )
+      })
       return
     }
 
--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,42 +1,17 @@
+const { exec } = require('../db/mysql')
+
 const getList = (author, keyword) => {
-  // 先返回假数据 （格式是正确的）
-  return [
-    {
-      id: 1,
-      title: '行宫',
-      content: '寥落古行宫，宫花寂寞红。白头宫女在，闲坐说玄宗。',
-      createTime: 1546610491112,
-      author: '元稹'
-    },
-    {
-      id: 2,
-      title: '登鹳雀楼',
-      content: '白日依山尽，黄河入海流。欲穷千里目，更上一层楼。',
-      createTime: 1598964604902,
-      author: '王之涣'
-    },
-    {
-      id: 3,
-      title: '新嫁娘词',
-      content: '白三日入厨下，洗手作羹汤。未谙姑食性，先遣小姑尝。',
-      createTime: 1598967923939,
-      author: '王建'
-    },
-    {
-      id: 4,
-      title: '相思',
-      content: '红豆生南国，春来发几枝。愿君多采撷，此物最相思。',
-      createTime: 1598964604902,
-      author: '王维'
-    },
-    {
-      id: 5,
-      title: '杂诗',
-      content: '君自故乡来，应知故乡事。来日绮窗前，寒梅著花未？',
-      createTime: 1598967910447,
-      author: '王维'
-    }
-  ]
+  let sql = `select * from blogs where 1=1 `
+  if (author) {
+    sql += `and author='${author}' `
+  }
+  if (keyword) {
+    sql += `and title like '%${keyword}%' `
+  }
+  sql += `order by createtime desc;`
+
+  // 返回 promise
+  return exec(sql)
 }
 
 const getDetail = (id) => {
